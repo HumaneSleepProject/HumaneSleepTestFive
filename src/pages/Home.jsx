4860b3ea183dede8cf4721d2eec7993209fbd809
Abhi Vishwas <34084368+AbhiVishwas@users.noreply.github.com>
2023-11-17 00:48:37 -0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,9 +4,9 @@ import { selectData } from "../pages/homeSlice";
 // Components
 import Hero from "../components/Hero";
 import AboutMe from "../components/AboutMe";
+import Contact from "../components/Contact";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
-import Contact from "../components/Contact";
 import { BackToTop } from "../components/globalStyledComponents";
 import Footer from "../components/Footer";
 
