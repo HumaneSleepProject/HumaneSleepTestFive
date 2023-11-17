f211aab7e4c7d115e7ca95aca0c7aaf2ad60caae
Abhi Vishwas <34084368+AbhiVishwas@users.noreply.github.com>
2023-11-17 00:45:36 -0800

    Update AboutMe.jsx
    added group photo section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -17,12 +17,32 @@ const StyledAboutMe = styled.section`
     width: 18rem;
     height: 18rem;
   }
-  .custom-img-style {
-    max-width: 15rem;
-    max-height: 15rem;
+`;
+
+const StyledImageSection = styled.section`
+  .image-container {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-top: 3rem;
+  }
+
+  .image-item {
+    margin: 1rem;
+    text-align: center;
+  }
+
+  .image {
     width: 100%;
+    max-width: 200px;
     height: auto;
-    /* Add any additional styling you need */
+    border-radius: 8px;
+  }
+
+  .description {
+    margin-top: 1rem;
+    font-size: 1rem;
   }
 `;
 
@@ -43,7 +63,7 @@ export default function AboutMe() {
             <Col className="d-flex flex-column text-center">
               <Container>
                 <p>{bio}</p>
-                {moreInfo && <p>{moreInfo}</p>}
+                {/* ... Additional content ... */}
               </Container>
             </Col>
             <Col className="d-none d-md-block text-center">
@@ -51,12 +71,36 @@ export default function AboutMe() {
                 src={GroupHSP}
                 alt="GroupHSP"
                 loading="lazy"
-                className="img-fluid custom-img-style"
+                className="img-fluid"
+                style={{ width: "15rem", height: "15rem" }}
               />
             </Col>
           </Row>
         </Container>
       </StyledAboutMe>
+
+      {/* New section for images and descriptions */}
+      <StyledImageSection>
+        <Container>
+          <Title>
+            <h2>Our Work</h2>
+            <div className="underline"></div>
+          </Title>
+          <Row className="image-container">
+            {/* Image 1 */}
+            <Col className="image-item">
+              <img src={/* Image source */} alt="Image 1" className="image" />
+              <p className="description">Description for Image 1</p>
+            </Col>
+            {/* Repeat for Images 2 to 6 */}
+            {/* Image 2 */}
+            {/* Image 3 */}
+            {/* Image 4 */}
+            {/* Image 5 */}
+            {/* Image 6 */}
+          </Row>
+        </Container>
+      </StyledImageSection>
     </Element>
   );
 }
